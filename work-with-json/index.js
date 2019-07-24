d8c3c2e9fe83492fe114d517c82a57cbf4fdbf22
AidaLuuppala <aida.luuppala@gmail.com>
2019-07-24 10:33:30 +0300

    get ids from first url, append second url with ids and fetch names from nested json

--- work-with-json/index.js
+++ work-with-json/index.js
@@ -7,22 +7,24 @@ app.use(bodyParser.json())
 var request = require("request")
 
 var url = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/organization/c5f6914f-302e-41cc-bed7-4d4215aac640"
-var ids = [];
+var url2 = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/"
 
 request({
-    url: url,
-    json: true
+	url: url,
+	json: true
 }, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-	
-	body.itemList.forEach(function(item){
-		ids.push(item.id)
-    	});
-    }
-    console.log(ids)
+	if (!error && response.statusCode === 200) {
+		body.itemList.forEach(function(item){
+			var urlWithId = url2 + item.id
+			request({
+				url: urlWithId,
+				json: true
+			}, function (error, response, body) {
+    				if (!error && response.statusCode === 200) {
+					var name = body.serviceChannelNames[0].value
+					console.log(name)
+    				}
+			})
+    		});
+    	}
 })
-
-
-
-
