e67f85d3f45cece9862bea41424a233fc7ee553c
AidaLuuppala <aida.luuppala@gmail.com>
2019-07-24 14:14:58 +0300

    push all names and translations to an object

--- work-with-json/index.js
+++ work-with-json/index.js
@@ -8,23 +8,28 @@ var request = require("request")
 
 var url = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/organization/c5f6914f-302e-41cc-bed7-4d4215aac640"
 var url2 = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/"
+var object = {}
+var key = "serviceChannelNames"
+object[key] = []
 
 request({
-	url: url,
-	json: true
+        url: url,
+        json: true
 }, function (error, response, body) {
-	if (!error && response.statusCode === 200) {
-		body.itemList.forEach(function(item){
-			var urlWithId = url2 + item.id
-			request({
-				url: urlWithId,
-				json: true
-			}, function (error, response, body) {
-    				if (!error && response.statusCode === 200) {
-					var name = body.serviceChannelNames[0].value
-					console.log(name)
-    				}
-			})
-    		});
-    	}
+        if (!error && response.statusCode === 200) {
+                body.itemList.forEach(function(item){
+                        var urlWithId = url2 + item.id
+                        request({
+                                url: urlWithId,
+                                json: true
+                        }, function (error, response, body) {
+                                if (!error && response.statusCode === 200) {
+                                        body.serviceChannelNames.forEach(function(item){
+                                                object[key].push(item.value)
+                                        })
+                                        console.log(object)
+                                }
+                        })
+                });
+        }
 })
