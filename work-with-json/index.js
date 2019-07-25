1372c3cdb549320d1eae3ed8c2548474ef1dd5cf
AidaLuuppala <aida.luuppala@gmail.com>
2019-07-25 14:38:18 +0300

    Check json data's languages and set right values to the object. Add filled objects to map with id as the key and object as the value.

--- work-with-json/index.js
+++ work-with-json/index.js
@@ -8,12 +8,10 @@ var request = require("request")
 
 //include file with the object
 var office = require('./data/office.js');
-office.id = 12345; //set id
-var officeId = office.id; //get id
-console.log(officeId);
 
 var url = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/organization/c5f6914f-302e-41cc-bed7-4d4215aac640"
 var url2 = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/"
+var offices = new Map();
 
 request({
         url: url,
@@ -27,9 +25,30 @@ request({
                                 json: true
                         }, function (error, response, body) {
                                 if (!error && response.statusCode === 200) {
-                                        body.serviceChannelNames.forEach(function(item){
-						console.log(item.value)
-                                        })
+                                        //set id
+                                        office.id = body.id
+                                        //console.log(office.id)
+                                        //check language and set names
+                                        for (var i = 0; i < body.serviceChannelNames.length; i++){
+                                                if (body.serviceChannelNames[i].language == 'fi'){
+                                                        office.name_fi = body.serviceChannelNames[i].value
+                                                }else if (body.serviceChannelNames[i].language == 'sv') {
+                                                        office.name_sv = body.serviceChannelNames[i].value
+                                                }
+                                        }
+                                        //console.log(office.name_fi)
+                                        //check language and set descriptions
+                                        for (var i = 0; i < body.serviceChannelDescriptions.length; i++){
+                                                if (body.serviceChannelDescriptions[i].language == 'fi' && body.serviceChannelDescriptions[i].type == 'Description'){
+                                                        office.description_fi = body.serviceChannelDescriptions[i].value
+                                                }else if (body.serviceChannelDescriptions[i].language == 'sv' && body.serviceChannelDescriptions[i].type == 'Description') {
+                                                        office.description_sv = body.serviceChannelDescriptions[i].value
+                                                }
+                                        }
+                                        //console.log(office.description_fi)
+                                        //set office objects to map offices with office.id as the key
+                                        offices.set(office.id, office);
+                                        console.log(offices);
                                 }
                         })
                 });
