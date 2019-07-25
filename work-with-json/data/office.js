ea447090eb091fb09a9495a509adc6474f4a457d
AidaLuuppala <aida.luuppala@gmail.com>
2019-07-25 11:48:26 +0300

    javascript object with getters and setters added and tested

---
+++ work-with-json/data/office.js
@@ -0,0 +1,94 @@
+var office = {
+	id : "",
+	name_sv : "",
+	name_fi : "",
+	description_sv : "",
+	description_fi : "",
+	street_sv : "",
+	street_fi : "",
+	street_number : "",
+	postal_code : "",
+	post_office_sv : "",
+	post_office_fi : "",
+	latitude : "",
+	longitude : "",
+	get id() {
+    		return this.id;
+  	},
+  	set id(id) {
+    		this.id = id;
+  	},
+	get name_sv() {
+                return this.name_sv;
+        },
+        set name_sv(name_sv) {
+                this.name_sv = name_sv;
+        },
+	get name_fi() {
+                return this.name_fi;
+        },
+        set name_fi(name_fi) {
+                this.name_fi = name_fi;
+        },
+	get description_sv() {
+                return this.description_sv;
+        },
+        set description_sv(description_sv) {
+                this.description_sv = description_sv;
+        },
+	get description_fi() {
+                return this.description_fi;
+        },
+        set description_fi(description_fi) {
+                this.description_fi = description_fi;
+        },
+	get street_sv() {
+                return this.street_sv;
+        },
+        set street_sv(street_sv) {
+                this.street_sv = street_sv;
+        },
+	get street_fi() {
+                return this.street_fi;
+        },
+        set street_fi(street_fi) {
+                this.street_fi = street_fi;
+        },
+	get street_number() {
+                return this.street_number;
+        },
+        set street_number(street_number) {
+                this.street_number = street_number;
+        },
+	get postal_code() {
+                return this.postal_code;
+        },
+        set postal_code(postal_code) {
+                this.postal_code = postal_code;
+        },
+	get post_office_sv() {
+                return this.post_office_sv;
+        },
+        set post_office_sv(post_office_sv) {
+                this.post_office_sv = post_office_sv;
+        },
+	get post_office_fi() {
+                return this.post_office_fi;
+        },
+        set post_office_fi(post_office_fi) {
+                this.post_office_fi = post_office_fi;
+        },
+	get latitude() {
+                return this.latitude;
+        },
+        set latitude(latitude) {
+                this.latitude = latitude;
+        },
+	get longitude() {
+                return this.longitude;
+        },
+        set longitude(longitude) {
+                this.longitude = longitude;
+        }
+};
+
--- work-with-json/index.js
+++ work-with-json/index.js
@@ -6,11 +6,14 @@ app.use(bodyParser.json())
 
 var request = require("request")
 
+//include file with the object
+var office = require('./data/office.js');
+office.id = 12345; //set id
+var officeId = office.id; //get id
+console.log(officeId);
+
 var url = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/organization/c5f6914f-302e-41cc-bed7-4d4215aac640"
 var url2 = "https://api.palvelutietovaranto.suomi.fi/api/v8/ServiceChannel/"
-var object = {}
-var key = "serviceChannelNames"
-object[key] = []
 
 request({
         url: url,
@@ -25,9 +28,8 @@ request({
                         }, function (error, response, body) {
                                 if (!error && response.statusCode === 200) {
                                         body.serviceChannelNames.forEach(function(item){
-                                                object[key].push(item.value)
+						console.log(item.value)
                                         })
-                                        console.log(object)
                                 }
                         })
                 });
